ed0a7d668395badc3737f5311e35d0b616788470
tr3sl9 <simonenko.mv@dozen.mephi.ru>
2025-05-11 16:20:35 +0300

    refactor: use DynamicArray my_struct

---
+++ 3b/my_struct.h
@@ -0,0 +1,17 @@
+#ifndef MY_STRUCT_H
+#define MY_STRUCT_H
+
+typedef struct {
+	void * array;
+	size_t capacity;
+	size_t count; 
+	size_t size_of_one;
+} DynamicArray;
+
+DynamicArray *da_create(const size_t size_of_one);
+int da_append(DynamicArray*, const void *item);
+void da_print(const DynamicArray * const, void(*print_item)(const void *));
+void da_free(DynamicArray * const);
+int da_delete_el(DynamicArray *da, size_t index);
+
+#endif
