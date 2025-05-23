5d0e590fa2158b04a0da0ec9a982267443c76970
tr3sl9 <simonenko.mv@dozen.mephi.ru>
2025-05-23 22:48:16 +0300

    feat: add create tree and delete in tree

--- 4a/src/lib/bst_lib.c
+++ 4a/src/lib/bst_lib.c
@@ -4,6 +4,15 @@
 #include "bst_lib.h"
 #include "../client/info_struct.h"
 
+BST *create_tree() {
+    BST *tree = (BST*)calloc(1, sizeof(BST));
+    if (!tree) {
+        return NULL;
+    }
+
+    return tree;
+}
+
 TreeNode *create_tree_node(const size_t key, const Info * const info) {
     TreeNode *node = (TreeNode*)calloc(1, sizeof(TreeNode));
     if (!node) {
@@ -23,6 +32,22 @@ TreeNode *create_tree_node(const size_t key, const Info * const info) {
     }
 
     return node;
+}
+
+void free_key_and_info(TreeNode * const node) {
+    free(node->key);
+    info_free(node->info);
+    return;
+}
+
+void set_key_and_info(TreeNode * const node, const size_t key, const Info * const info) {
+    if (!node || !key || !info) {
+        return TREE_VAL;
+    }
+
+    node->key = key;
+    node->info = info;
+    return;
 }
 
 tree_err insert_tree(BST * const tree, const size_t key, const Info * const info) {
@@ -62,4 +87,63 @@ tree_err insert_tree(BST * const tree, const size_t key, const Info * const info
     new_node->parent = current_parent;
     
     return TREE_OK;
-} 
+}
+
+tree_err delete_tree(BST * const tree, const size_t key) {
+    if (!tree) {
+        return TREE_NULL;
+    }
+    if (!key) {
+        return TREE_VAL;
+    }
+
+    TreeNode *current = tree->root;
+    if (!current) {
+        return TREE_EMPTY;
+    }
+    TreeNode *current_parent = NULL;
+
+    while (!current) {
+        if (key > current->key) {
+            current_parent = current;
+            current = current->right;
+        } else if (key < current->key) {
+            current_parent = current;
+            current = current->left;
+        } else {
+            break;
+        }
+    }
+
+    if (!current) {
+        return TREE_VAL;
+    }
+
+    if (current->left && current->right) {
+        TreeNode *child = current->right;
+        TreeNode *child_parrent = current;
+        while (child->left) {
+            child_parent = child;
+            child = child->left;
+        }
+        free_key_and_info(current);
+        set_key_and_info(current, child->key, child->info);
+        child_parrent->left = child->right;
+        free(child);
+    } else {
+        free_key_and_info(current);
+        TreeNode *child = (current->left) ? current->left : current->right;
+        if (current_parent) {
+            if (current_parent->left == current) {
+                current_parent->left = child;
+            } else {
+                current_parent->right = child;
+            }
+        } else {
+            tree->root = child;
+        }
+        free(current);
+    }
+
+    return TREE_OK;
+}
