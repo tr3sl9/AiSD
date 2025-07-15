549820df7487ba4b46d0c85490eb62be882f1602
simonenko.mv <simonenko.mv@dozen.mephi.ru>
2025-07-15 14:50:12 +0300

    refactor: benchmark with logarithm

--- 4a/src/timing/bst_benchmark.c
+++ 4a/src/timing/bst_benchmark.c
@@ -4,16 +4,93 @@
 #include "../lib/bst_lib.h"
 #include "../client/info_struct.h"
 
-#define NUM_SIZES 30
+#define insert 1
+#define delete 2
+#define find 3
+
+#define NUM_SIZES 50
 #define NUM_ITER 20
-#define NUM_OPS 1000
 
 #define NANO 1000000000
-#define DEF_COUNT_KEYS 40000
+#define DEF_COUNT_KEYS 100000
 #define ITER_COUNT_KEYS 1000
 
 double time_delta(const struct timespec *start, const struct timespec *end) {
     return end->tv_sec * NANO + end->tv_nsec - start->tv_sec * NANO - start->tv_nsec;
+}
+
+void default_insert_tree(BST * const tree) {
+    if(!tree) {
+        return;
+    }
+
+    for (size_t j = 0; j < DEF_COUNT_KEYS; j++) {
+        size_t key = rand();
+        Info *info = info_create(rand());
+        insert_tree(tree, key, info); 
+    }
+
+    return;
+}
+
+double measuring_time(BST * const tree, size_t *key_arr, Info **info_arr, size_t *idx_arr, int op) {
+    struct timespec tp_start;
+    struct timespec tp_end;
+
+    switch (op) {
+        case insert:
+            clock_gettime(CLOCK_MONOTONIC, &tp_start);
+            for (size_t j = 0; j < ITER_COUNT_KEYS; j++) {
+                insert_tree(tree, key_arr[j], info_arr[j]);
+            }
+            clock_gettime(CLOCK_MONOTONIC, &tp_end);
+            return time_delta(&tp_start, &tp_end);
+
+        case find:
+            clock_gettime(CLOCK_MONOTONIC, &tp_start);
+            for (size_t j = 0; j < ITER_COUNT_KEYS; j++) {
+                find_tree(tree, key_arr[idx_arr[j]]);
+            }
+            clock_gettime(CLOCK_MONOTONIC, &tp_end);
+            return time_delta(&tp_start, &tp_end);
+
+        case delete:
+            clock_gettime(CLOCK_MONOTONIC, &tp_start);
+            for (size_t j = 0; j < ITER_COUNT_KEYS; j++) {
+                delete_tree(tree, key_arr[j]);
+            }
+            clock_gettime(CLOCK_MONOTONIC, &tp_end);
+            return time_delta(&tp_start, &tp_end);
+    }
+    return 0;
+}
+
+double iteration_insert_tree(BST * const tree, size_t *key_arr, Info **info_arr) {
+    if (!tree || !key_arr || !info_arr) {
+        return 0;
+    }
+    for (size_t j = 0; j < ITER_COUNT_KEYS; j++) {
+        info_arr[j] = info_create(rand());
+        key_arr[j] = rand();
+    }
+    return measuring_time(tree, key_arr, info_arr, NULL, insert);
+}
+
+double iteration_find_tree(BST * const tree, size_t *key_arr, size_t *idx_arr) {
+    if (!tree || !key_arr || !idx_arr) {
+        return 0;
+    }
+    for (size_t j = 0; j < ITER_COUNT_KEYS; j++) {
+        idx_arr[j] = rand() % ITER_COUNT_KEYS;
+    }
+    return measuring_time(tree, key_arr, NULL, idx_arr, find);
+}
+
+double iteration_delete_tree(BST * const tree, size_t *key_arr) {
+    if (!tree || !key_arr) {
+        return 0;
+    }
+    return measuring_time(tree, key_arr, NULL, NULL, delete);
 }
 
 int main() {
@@ -29,53 +106,50 @@ int main() {
     
     for (size_t s = 0; s < NUM_SIZES; s++) {
 
-        //заполняю на 90 000 каждый раз
-        for (size_t j = 0; j < DEF_COUNT_KEYS; j++) {
-            size_t key = rand();
-            Info *info = info_create(rand());
-            insert_tree(tree, key, info); 
-        }
+        default_insert_tree(tree);
 
         double time_insert = 0;
         double time_delete = 0;
         double time_search = 0;
 
         for (size_t i = 0; i < NUM_ITER; i++) {
-            size_t *keys = malloc(ITER_COUNT_KEYS * sizeof(size_t));
-            if (!keys) {
+            size_t *key_arr = malloc(ITER_COUNT_KEYS * sizeof(size_t));
+            if (!key_arr) {
                 printf("Memory allocation failed\n");
+                free_tree(tree);
                 fclose(file);
                 return 1;
             }
-            // Вставка
-            struct timespec tp_start;
-            struct timespec tp_end;
-            clock_gettime(CLOCK_MONOTONIC, &tp_start);
-            for (size_t j = 0; j < ITER_COUNT_KEYS; j++) {
-                size_t key = rand();
-                Info *info = info_create(rand());
-                keys[j] = key;
-                insert_tree(tree, key, info); 
-            }
-            clock_gettime(CLOCK_MONOTONIC, &tp_end);
-            time_insert += time_delta(&tp_start, &tp_end);
-            // Поиск
-            clock_gettime(CLOCK_MONOTONIC, &tp_start);
-            for (size_t j = 0; j < ITER_COUNT_KEYS; j++) {
-                int idx = rand() % ITER_COUNT_KEYS;
-                find_tree(tree, keys[idx]);
-            }
-            clock_gettime(CLOCK_MONOTONIC, &tp_end);
-            time_search += time_delta(&tp_start, &tp_end);
-            // Удаление
-            clock_gettime(CLOCK_MONOTONIC, &tp_start);
-            for (size_t j = 0; j < ITER_COUNT_KEYS; j++) {
-                delete_tree(tree, keys[j]);
-            }
-            clock_gettime(CLOCK_MONOTONIC, &tp_end);
-            time_delete += time_delta(&tp_start, &tp_end);
 
-            free(keys);
+            Info **info_arr = malloc(ITER_COUNT_KEYS * sizeof(Info*));
+            if (!info_arr) {
+                printf("Memory allocation failed\n");
+                free(key_arr);
+                free_tree(tree);
+                fclose(file);
+                return 1;
+            }
+
+            // Вставка
+            time_insert += iteration_insert_tree(tree, key_arr, info_arr);
+            // Поиск
+            size_t *idx_arr = malloc(ITER_COUNT_KEYS * sizeof(size_t));
+            if (!idx_arr) {
+                printf("Memory allocation failed\n");
+                free(info_arr);
+                free(key_arr);
+                free_tree(tree);
+                fclose(file);
+                return 1;
+            }
+
+            time_search += iteration_find_tree(tree, key_arr, idx_arr);
+            // Удаление
+            time_delete += iteration_delete_tree(tree, key_arr);
+
+            free(info_arr);
+            free(key_arr);
+            free(idx_arr);
         }
         // Записать средние значения по размеру дерева
         fprintf(file, "%zu;%.6f;%.6f;%.6f\n", (s + 1) * DEF_COUNT_KEYS, time_insert/(double)NUM_ITER, time_search/(double)NUM_ITER, time_delete/(double)NUM_ITER);
